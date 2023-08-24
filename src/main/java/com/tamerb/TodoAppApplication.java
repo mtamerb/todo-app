9f3534b5a5e3d685ce68e482420f676e3840ee2e
tamer bilici <tamerbilici00@gmail.com>
2023-08-24 18:18:30 +0300

    change package

--- src/main/java/com/tamerb/TodoAppApplication.java
+++ src/main/java/com/tamerb/TodoAppApplication.java
@@ -1,4 +1,4 @@
-package com.example.todoapp;
+package com.tamerb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/tamerb/TodoAppApplicationTests.java
+++ src/test/java/com/tamerb/TodoAppApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.todoapp;
+package com.tamerb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
